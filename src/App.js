3ef6cc019d7e7727d7999f9c995d81a8af64d625
Timothy Jones <tim.jones@versent.com.au>
2020-05-11 12:00:15 +1000

    feat(example): Add example hook code from React tutorial at https://reactjs.org/docs/faq-ajax.html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import MyComponent from './MyComponent';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <MyComponent />
         <a
           className="App-link"
           href="https://reactjs.org"
